5f078f5150ec1bc860a8678e953143a31e44b70d
zhikh <zhikh.k@gmail.com>
2024-03-30 17:03:40 +0300

    refactor handlers

--- handlers/find_handler.py
+++ handlers/find_handler.py
@@ -12,38 +12,42 @@ from handlers.states import States
 router = Router()
 
 
-ANS_FIND__BY_ID   = "По ID"
+ANS_FIND__BY_ID = "По ID"
 ANS_FIND__BY_NAME = "По названию"
 ANS_FIND__BY_TAGS = "По тегам"
 
+ANS_YES = "Да"
+ANS_NO = "Нет"
 
-by_id_btn   = KeyboardButton(text=ANS_FIND__BY_ID)
+
+by_id_btn = KeyboardButton(text=ANS_FIND__BY_ID)
 by_name_btn = KeyboardButton(text=ANS_FIND__BY_NAME)
 by_tags_btn = KeyboardButton(text=ANS_FIND__BY_TAGS)
-
+yes_btn = KeyboardButton(text=ANS_YES)
+no_btn = KeyboardButton(text=ANS_NO)
 
 
 @router.message(Command("find"))
 async def find__start(message: Message, state: FSMContext):
-    await state.set_state(States.find)
     await message.answer(
         "Готов найти ресурс любым возможным способом!\n",
         reply_markup=ReplyKeyboardMarkup(keyboard=[
-            [ by_id_btn ],
-            [ by_name_btn ],
-            [ by_tags_btn ],
+            [by_id_btn],
+            [by_name_btn],
+            [by_tags_btn],
         ])
     )
+    await state.set_state(States.find__start)
 
 
-
-@router.message(States.find, F.text == ANS_FIND__BY_ID)
+# ID ==========================================================================
+@router.message(States.find__start, F.text == ANS_FIND__BY_ID)
 async def find__by_id(message: Message, state: FSMContext):
-    await state.set_state(States.find__wait_id)
     await message.answer(
         "Введите id ресурса:\n",
         reply_markup=ReplyKeyboardRemove(),
     )
+    await state.set_state(States.find__wait_id)
 
 
 @router.message(States.find__wait_id)
@@ -54,48 +58,39 @@ async def find__enter_id(message: Message, state: FSMContext):
             "К сожалению, это не похоже на ID ресурса :(\n"
             "Попробуйте ещё раз или используйте команду /cancel"
         )
-    
-    await state.set_state(default_state)
     res_id = int(message.text)
-    await message.answer(
-        f"Выполняю поиск по id={res_id}...",
-        reply_markup=ReplyKeyboardRemove()
-    )
-    time.sleep(1)
-    await message.reply("*Делаю вид, что что-то нашёл*")
+    await state.update_data({ "find__id": res_id })
+    await __find__process(message, state)
+# =============================================================================
 
 
-
-@router.message(States.find, F.text == ANS_FIND__BY_NAME)
+# NAME ========================================================================
+@router.message(States.find__start, F.text == ANS_FIND__BY_NAME)
 async def find__by_name(message: Message, state: FSMContext):
-    await state.set_state(States.find__wait_name)
     await message.answer(
         "Введите название ресурса или его часть:\n",
         reply_markup=ReplyKeyboardRemove(),
     )
+    await state.set_state(States.find__wait_name)
 
 
 @router.message(States.find__wait_name)
 async def find__enter_name(message: Message, state: FSMContext):
     assert message.text
-    await state.set_state(default_state)
     res_name = message.text
-    await message.answer(
-        f"Выполняю поиск по имени \'{res_name}\'...",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-    time.sleep(1)
-    await message.answer("*Делаю вид, что что-то нашёл*")
+    await state.update_data({ "find__name": res_name })
+    await __find__process(message, state)
+# =============================================================================
 
 
-
-@router.message(States.find, F.text == ANS_FIND__BY_TAGS)
+# TAGS ========================================================================
+@router.message(States.find__start, F.text == ANS_FIND__BY_TAGS)
 async def find__by_tags(message: Message, state: FSMContext):
-    await state.set_state(States.find__wait_tags)
     await message.answer(
         "Введите #теги через пробел:\n",
         reply_markup=ReplyKeyboardRemove(),
     )
+    await state.set_state(States.find__wait_tags)
 
 
 @router.message(States.find__wait_tags)
@@ -107,18 +102,39 @@ async def find__enter_tags(message: Message, state: FSMContext):
             return await message.reply(
                 f"\"{tag}\" не похоже на тег :( Попробуйте ещё раз"
             )
-    await state.set_state(default_state)
+    await state.update_data({ "find__tags": res_tags })
+    await __find__process(message, state)
+# =============================================================================
+
+
+
+async def __find__process(message: Message, state: FSMContext):
+    data = await state.get_data()
+    res_id: int = data.get("find__id", None) 
+    res_name: str = data.get("find__name", None)
+    res_tags: list[str] = data.get("find__tags", [])
+
+    reply_text = "Выполняю поиск по:\n"
+    if res_id:
+        reply_text += f"ID: {res_id}\n"
+    if res_name:
+        reply_text += f"Имени: {res_name}\n"
+    if res_tags:
+        res_tags_text = ", ".join(res_tags) + "\n"
+        reply_text += f"Тегам: {res_tags_text}"
+
     await message.reply(
-        f"Выполняю поиск по тегам: " + \
-        ", ".join([f"{tag}" for tag in res_tags]),
+        reply_text,
         reply_markup=ReplyKeyboardRemove(),
     )
+    
     time.sleep(1)
+    
     await message.reply("*Делаю вид, что что-то нашёл*")
+    await state.clear()
 
 
-
-@router.message(States.find)
+@router.message(States.find__start)
 async def find__unknown(message: Message, state: FSMContext):
     await message.answer(
         "Я не понимаю Вас :( Попробуйте ещё раз",
--- handlers/states.py
+++ handlers/states.py
@@ -2,7 +2,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class States(StatesGroup):
-    find = State()
+    find__start = State()
     find__wait_id = State()
     find__wait_name = State()
     find__wait_tags = State()
